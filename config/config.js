01124ed7f48a732f64ddbdefedfe35c76fb29760
asaad2121 <md.asaad.shaikh@gmail.com>
2025-10-03 22:03:28 +1300

    Models config

--- config/config.js
+++ config/config.js
@@ -6,20 +6,27 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+        dialect: process.env.DB_DIALECT || 'mysql',
     },
     test: {
         username: process.env.DB_TEST_USERNAME,
         password: process.env.DB_TEST_PASSWORD,
         database: process.env.DB_TEST_DATABASE,
         host: process.env.DB_TEST_HOST,
-        dialect: process.env.DB_TEST_DIALECT,
+        port: process.env.DB_TEST_PORT ? Number(process.env.DB_TEST_PORT) : 3306,
+        dialect: process.env.DB_TEST_DIALECT || 'mysql',
     },
     production: {
         username: process.env.DB_PRODUCTION_USERNAME,
         password: process.env.DB_PRODUCTION_PASSWORD,
         database: process.env.DB_PRODUCTION_DATABASE,
         host: process.env.DB_PRODUCTION_HOST,
-        dialect: process.env.DB_PRODUCTION_DIALECT,
+        port: process.env.DB_PRODUCTION_PORT ? Number(process.env.DB_PRODUCTION_PORT) : 3306,
+        dialect: process.env.DB_PRODUCTION_DIALECT || 'mysql',
+        dialectOptions: {
+            connectTimeout: 30000,
+        },
+        logging: false, // disable SQL logs in prod
     },
 };
--- models/index.js
+++ models/index.js
@@ -10,11 +10,13 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+    dialectOptions: config.dialectOptions,
+    logging: config.logging,
+});
 
 fs.readdirSync(__dirname)
     .filter((file) => {
